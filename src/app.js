031a2cf72d942ed9ad7f4a062c74a62d34f04964
JitendraRay <jkjohnson552@gmail.com>
2024-04-25 17:49:09 +0530

    register router

--- src/app.js
+++ src/app.js
@@ -20,5 +20,15 @@ app.use(express.static("public")) // for storing asset statically
 
 app.use(cookieParser()) // to access & set user's broswer's cookie
 
+// router
+
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+
+app.use('/api/v1/users',userRouter)
+
+
 
 export { app };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+
+cloudinary.config({
+    cloud_name:process.env.cloud_name,
+    api_key:process.env.api_key,
+    api_secret:process.env.api_secret
+})
+
+const uploadOnCloudinary = async (localFilePath) =>{
+  try {
+    if(!localFilePath) return null    
+    // upload the file on cloudinary
+     const response = await cloudinary.uploader.upload(localFilePath,{
+      resource_type:"auto"
+    })
+    //file has been upload successfully
+    console.log("file is upload on cloudinary");
+    console.log(response.url);
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload operation got filed
+    return null;
+  }
+}
+
+
+export {uploadOnCloudinary}
+
+
+
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
